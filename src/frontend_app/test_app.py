c165de176201888adc8c5cbdca0339148847d2d6
momoko <momoko.yamada@woven.toyota>
2024-08-01 08:17:41 +0900

    Modified utilizing dataframe

--- src/frontend_app/test_app.py
+++ src/frontend_app/test_app.py
@@ -1,13 +1,31 @@
 import streamlit as st
 import pandas as pd
+import sqlite3
 
+# SQLiteデータベースファイルへのパス
+db_path = 'cooking_system.db'  # データベースファイルのパスを指定します
+
+# SQLiteデータベースに接続
+conn = sqlite3.connect(db_path)
+
+# SQLクエリを使って'Refrigerator'のテーブルをDataFrameとして読み込む
+table_name = 'Refrigerator'  # 取得したいテーブルの名前を指定します
+query = f'SELECT * FROM {table_name}'
+# Pandasを使ってテーブルを読み込み
+ref_df = pd.read_sql_query(query, conn)
+
+
+# SQLクエリを使って'FoodData'のテーブルをDataFrameとして読み込む
+table_name = 'FoodData'  # 取得したいテーブルの名前を指定します
+query = f'SELECT * FROM {table_name}'
+# Pandasを使ってテーブルを読み込み
+food_df = pd.read_sql_query(query, conn)
+
+# Streamlitを使ってDataFrameを表示
 st.title('Test')
 st.caption('This is the test apprication.')
-
-
-ref_df = pd.read_json('refrigirator.json')
-food_df = pd.read_json('fooddata.json')
 view_df = pd.merge(ref_df, food_df, how='inner')
-
-
 st.dataframe(view_df)
+
+# 接続を閉じる
+conn.close()
