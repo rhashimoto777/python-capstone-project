0fc74555b455eef560e5a198a44133fcc37566e8
momokowv <161792955+momokowv@users.noreply.github.com>
2024-08-05 09:04:11 +0900

    Pcpg 11 to fe main (#14)
    * Test script for internal discussion
    
    * Modified utilizing dataframe
    
    * Modify the path
    
    * Remove the test database
    
    * Modify the code
    
    * Remove unused function
    
    * Add the class

--- src/frontend_main.py
+++ src/frontend_main.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import streamlit as st
+import pandas as pd
 
 # subfolderをモジュール検索パスに追加
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -26,3 +27,26 @@ class FrontEndOperator():
             st.header(f"Table: {table}")
             df = self.df_dict[table]
             st.dataframe(df)
+
+class FrontEndOperatorRefrigeratorFooddata():
+    def __init__(self, initial_df_dict, second_df_dict) -> None:
+        self.df_dict1 = initial_df_dict
+        self.df_dict2 = second_df_dict
+        return
+
+    def update_df_dict(self, df_dict1, df_dict2):
+        self.df_dict1 = df_dict1
+        self.df_dict2 = df_dict2
+        return
+    
+    def show_refrigerator_fooddata_df(self, df_dict_refrigerator, df_dict_fooddata) :
+        
+        # Streamlitを使ってDataFrameを表示
+        st.title('冷蔵庫の中にある食材の種類・数')
+        st.caption('「Refrigerator」内にある食材の情報を、「Refrigerator」」と「FoodData」のDataframeを参照して、UI上に表示する。')
+        df_dict_refrigerator_fooddata = pd.merge(df_dict_refrigerator, df_dict_fooddata, how='inner')
+        st.dataframe(df_dict_refrigerator_fooddata)
+        return
+
+
+
