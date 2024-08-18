f035b28150203322b621cfe4fb0b88a9948d1a0f
rhashimoto <ryuta.hashimoto@woven-planet.global>
2024-08-19 00:49:32 +0900

    s

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,18 @@
 import os
 import sys
 
+from src.backend_app import backend_common as common
+
 # プロジェクトのルートディレクトリを追加
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+def pytest_configure(config):
+    """
+    全てのテストの計算よりも先に実行される関数
+    """
+    # PYTEST用のDataBaseに切り替える
+    common.init("_PYTEST_")
+    print(".\n.\n.\n")
+    print(f"......... pytest用のDataBaseに切り替えます。 {common.DB_PATH} .........")
+    print(".\n.\n.\n")
+    return
